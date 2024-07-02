3428673cef602db10b6ce19aebb14ed2963d0db4
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-07-02 15:20:08 +0200

    CRUD complete

--- app.js
+++ app.js
@@ -48,5 +48,17 @@ app.post('/api/books', (req, res, next) => {
       .then(book => res.status(200).json(book))
       .catch(error => res.status(404).json({ error }));
   });
+// PUT => Modification d'un livre existant
+  app.put('/api/books/:id', (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+  // DELETE => Suppression d'un livre
+  app.delete('/api/books/:id', (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
 module.exports = app;
