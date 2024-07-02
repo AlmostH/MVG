c216688f10bb793810924f09fb2bd37398b4412c
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-07-02 11:25:34 +0200

    connect mogodb database and creation of first shema

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+mongoose.connect('mongodb+srv://MonVieuxGrimoire:w53IEU1EGXcIDonr@cluster0.srwo35g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.status(201);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+app.post('/api/books', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
 });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+app.use('/api/books', (req, res, next) => {
+  const books = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      auteur: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      annee:2022,
+      genre: 'policier',
+      note: 4,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      auteur: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      annee: 2022,
+      genre: 'policier',
+      note: 4,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
