2c49c358eeed497ac1093530ab5b4c05ca38154d
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-07-04 16:19:06 +0200

    files modifications

--- models/Book.js
+++ models/Book.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  year: { type: Number, required: true },
-  category: { type: String, required: true },
-  note: { type: Number, required: true },
-  imageUrl: {type: String, required: true},
-});
+    userId: { type: String, required: true },
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
+    ratings: [
+      {
+          userId: { type: String },
+          grade: { type: Number },
+      }
+    ],
+    averageRating: { type: Number },
+  });
 
 module.exports = mongoose.model('Book', bookSchema);
--- routes/books.js
+++ routes/books.js
@@ -1,42 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-const Book = require('../models/Book');
+const bookCtrl = require('../controllers/books');
+
 
 // POST => Enregistrement d'un livre
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-      ...req.body
-    });
-    book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+router.post('/',bookCtrl.createBook );
   
   // GET => Récupération de tous les livres
-    router.get('/', (req, res, next) => {
-      Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({ error }));
-    });
+    router.get('/', bookCtrl.getAllBooks);
   // GET => Récupération d'un livre spécifique
-    router.get('/:id', (req, res, next) => {
-      Book.findOne({ _id: req.params.id })
-        .then(book => res.status(200).json(book))
-        .catch(error => res.status(404).json({ error }));
-    });
+    router.get('/:id', bookCtrl.getOneBook);
   // PUT => Modification d'un livre existant
-    router.put('/:id', (req, res, next) => {
-      Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-        .catch(error => res.status(400).json({ error }));
-    });
+    router.put('/:id', bookCtrl.modifyBook);
     // DELETE => Suppression d'un livre
-    router.delete('/:id', (req, res, next) => {
-      Book.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
-    });
+    router.delete('/:id', bookCtrl.deleteBook);
 
 module.exports = router;
