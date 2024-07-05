ee97eab58a172e5deede52de88d9863b39f0b917
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-07-05 09:34:21 +0200

    middleware auth ok

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const auth = require('auth');
 const router = express.Router();
+
+
 const bookCtrl = require('../controllers/books');
 
 
@@ -8,12 +10,12 @@ const bookCtrl = require('../controllers/books');
 router.post('/',bookCtrl.createBook );
   
   // GET => Récupération de tous les livres
-    router.get('/', bookCtrl.getAllBooks);
+    router.get('/', auth, bookCtrl.getAllBooks);
   // GET => Récupération d'un livre spécifique
-    router.get('/:id', bookCtrl.getOneBook);
+    router.get('/:id', auth, bookCtrl.getOneBook);
   // PUT => Modification d'un livre existant
-    router.put('/:id', bookCtrl.modifyBook);
+    router.put('/:id', auth, bookCtrl.modifyBook);
     // DELETE => Suppression d'un livre
-    router.delete('/:id', bookCtrl.deleteBook);
+    router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
