3f6cfcb63a52d239ac0de9ec714fc43e26b7f82f
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-07-10 16:32:33 +0200

    middleware resize image created

---
+++ middleware/sharp-config.js
@@ -0,0 +1,33 @@
+const sharp = require('sharp');
+const fs = require('fs');
+
+const resizeImage = (req, res, next) => {
+
+  if (!req.file) {
+    return next();
+  }
+
+
+  sharp(req.file.path)
+    .resize(206, 260, { fit: 'cover' }) 
+    .toFormat('webp')
+    .toFile('images/' + req.file.filename.replace(/\.(jpg|jpeg|png)$/, '.webp'))
+
+    .then(() => {
+
+      req.file.filename = req.file.filename.replace(/\.(jpg|jpeg|png)$/, '.webp');
+
+      fs.unlink(req.file.path, (err) => {
+        if (err) {
+          console.error('Error deleting original file:', err);
+        }
+        next();
+      });
+    })
+    .catch((error) => {
+      console.error('Image processing error:', error);
+      next();
+    });
+};
+
+module.exports = resizeImage;
--- routes/books.js
+++ routes/books.js
@@ -3,18 +3,19 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const resizeImage = require('../middleware/sharp-config');
 
 const bookCtrl = require('../controllers/books');
 
 
    // POST => Enregistrement d'un livre
-    router.post('/', auth, multer, bookCtrl.createBook );
+    router.post('/', auth, multer, resizeImage, bookCtrl.createBook );
   // GET => Récupération de tous les livres
     router.get('/', bookCtrl.getAllBooks);
   // GET => Récupération d'un livre spécifique
     router.get('/:id', bookCtrl.getOneBook);
   // PUT => Modification d'un livre existant
-    router.put('/:id', auth, multer, bookCtrl.modifyBook);
+    router.put('/:id', auth, multer, resizeImage, bookCtrl.modifyBook);
     // DELETE => Suppression d'un livre
     router.delete('/:id', auth, bookCtrl.deleteBook);
 
